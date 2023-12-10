60739e259379837022881d7fced71c0eb5b06178
Theronguard <theronguard100@gmail.com>
2023-12-10 21:03:57 +0100

    Add files via upload

---
+++ AudioPatcher.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using System.IO;
+using BepInEx;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CowboySwaps
+{
+    public static class AudioPatcher
+    {
+        public static AudioClip BepInLoadClip(string itemName)
+        {
+            List<string> list = Directory.GetDirectories(Paths.PluginPath, "CowboySwapsSFX", SearchOption.AllDirectories).ToList();
+            foreach (string item in list)
+            {
+                string newPath = item + "\\" + itemName;
+                if (File.Exists(newPath))
+                    return LoadClip(newPath);
+            }
+            return null;
+        }
+
+        public static AudioClip LoadClip(string path)
+        {
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("file://" + path, AudioType.WAV))
+            {
+                www.SendWebRequest();
+
+                while (!www.isDone);
+
+                if (www.result == UnityWebRequest.Result.Success)
+                    return DownloadHandlerAudioClip.GetContent(www);
+                else
+                    CowboySwaps.CustomLogger.LogError($"Failed to load WAV audio clip from {path}. Error: {www.error}");
+            }
+            return null;
+        }
+    }
+}
---
+++ CowboySwaps.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Unity;
+using UnityEngine.Audio;
+
+namespace CowboySwaps
+{
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class CowboySwaps : BaseUnityPlugin
+    {
+        private const string modGUID = "Theronguard.CowboySwaps";
+        private const string modName = "CowboySwaps";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static ManualLogSource CustomLogger;
+
+        //file id + file name
+        public static Dictionary<string,string> files = new Dictionary<string,string>();
+
+        void Awake()
+        {
+            files.Add("HornClose","ggn1.wav");
+            files.Add("HornFar", "ggn2.wav");
+            files.Add("JesterWindup", "jst.wav");
+
+            CustomLogger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            CustomLogger.LogInfo("Daniel works!");
+            harmony.PatchAll(typeof(CowboySwaps));
+            harmony.PatchAll(typeof(HornSourcePatch));
+            harmony.PatchAll(typeof(JesterSFXPatch));
+        }
+    }
+}
