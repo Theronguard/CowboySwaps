f60e13b85583eb9ccb8d074ba194bb7785117134
Theronguard <theronguard100@gmail.com>
2023-12-10 21:04:22 +0100

    Add files via upload

---
+++ Patches/HornSourcePatch.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace CowboySwaps
+{
+    [HarmonyPatch(typeof(ShipAlarmCord))]
+    public class HornSourcePatch : BaseUnityPlugin
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void PatchHorn(ShipAlarmCord __instance)
+        {
+            __instance.hornClose.clip = AudioPatcher.BepInLoadClip(CowboySwaps.files["HornClose"]);
+            __instance.hornFar.clip = AudioPatcher.BepInLoadClip(CowboySwaps.files["HornFar"]);
+
+            CowboySwaps.CustomLogger.LogWarning("Horn Patched!");
+        }
+    }
+
+    
+}
---
+++ Patches/JesterSFXPatch.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace CowboySwaps
+{
+    [HarmonyPatch(typeof(JesterAI))]
+    public class JesterSFXPatch : BaseUnityPlugin
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void PatchJesterSFX(JesterAI __instance)
+        {
+            __instance.popGoesTheWeaselTheme = AudioPatcher.BepInLoadClip(CowboySwaps.files["JesterWindup"]);
+            CowboySwaps.CustomLogger.LogWarning("Daniel Jester Patched!");
+        }
+    }
+
+    
+}
